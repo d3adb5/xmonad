5129efb2b75be0f50664ce489225123ab4d1a469
d3adb5 <me@d3adb5.net>
2024-07-28 04:30:38 -0700

    fix: apply unreleased steam flicker fix
    Apply an unreleased fix for the Steam context menu flicker issue.
    Hopefully in due time there will be a 0.18.1 release of xmonad-contrib
    that is included in a Stackage LTS snapshot.

--- src/Config/HandleEventHook.hs
+++ src/Config/HandleEventHook.hs
@@ -2,12 +2,14 @@ module Config.HandleEventHook (handleEventHook) where
 
 import XMonad hiding (handleEventHook)
 import XMonad.Layout.MagicFocus (followOnlyIf, disableFollowOnWS)
+import XMonad.Util.Hacks (fixSteamFlicker)
 
 import Data.Semigroup (All(..))
 
 handleEventHook :: Event -> X All
 handleEventHook
     = followOnlyIf (disableFollowOnWS ["dev"])
+  <+> fixSteamFlicker
   <+> defaultHEHook
 
 defaultHEHook :: Event -> X All
